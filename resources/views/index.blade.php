0896dbe379eaac230b5145d5743252d782997a68
Shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-04 10:34:38 +0100

    added paddig to form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,13 +8,13 @@
     <title>Chat</title>
 </head>
 <body class="bg-gray-900">
-    <div class="app flex flex-col justify-between h-screen">
-        <header class="flex flex-col place-items-center bg-gray-800 text-white p-4 drop-shadow">
+    <div class="app flex flex-col justify-between items-center h-screen">
+        <header class="flex flex-col place-items-center w-screen bg-gray-800 text-white p-4 drop-shadow">
             <span id="welcomeMessage"></span>
             <input class="rounded p-2 w-72 text-center text-black drop-shadow-lg focus:drop-shadow" type="text" name="username" id="username" placeholder="enter your name :)" autocomplete="off">
         </header>
         <div id="messages" class="overflow-y-auto"></div>
-        <form class="bg-gray-900 flex w-screen p-2 justify-evenly" id="messageForm">
+        <form class="bg-gray-900 w-screen flex p-2 pb-10 justify-evenly" id="messageForm">
             <input class="rounded resize-none w-screen text-center text-black drop-shadow-lg focus:drop-shadow" type="text" name="message" id="message" placeholder="write a message :)" autocomplete="off"></input>
             <button class="bg-gray-800 text-white py-3 px-6 hover:bg-gray-600 active:bg-gray-700 rounded drop-shadow-lg ml-3" type="submit" id="sendMessage">Send</button>
         </form>
