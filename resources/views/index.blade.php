83ab607f9b0c4047c181065fa9964add3a8c8786
Shaduwi <jp.vthoenderdaal@gmail.com>
2022-02-04 10:25:15 +0100

    removed flex grow

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
             <span id="welcomeMessage"></span>
             <input class="rounded p-2 w-72 text-center text-black drop-shadow-lg focus:drop-shadow" type="text" name="username" id="username" placeholder="enter your name :)" autocomplete="off">
         </header>
-        <div id="messages" class="overflow-y-auto flex-grow"></div>
+        <div id="messages" class="overflow-y-auto"></div>
         <form class="bg-gray-900 flex w-screen p-2 justify-evenly" id="messageForm">
             <input class="rounded resize-none w-screen text-center text-black drop-shadow-lg focus:drop-shadow" type="text" name="message" id="message" placeholder="write a message :)" autocomplete="off"></input>
             <button class="bg-gray-800 text-white py-3 px-6 hover:bg-gray-600 active:bg-gray-700 rounded drop-shadow-lg ml-3" type="submit" id="sendMessage">Send</button>
